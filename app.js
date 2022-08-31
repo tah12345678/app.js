4c93ea83ad3769a51b318f29bec73b8b6f291fd9
tah12345678 <111244936+tah12345678@users.noreply.github.com>
2022-08-31 10:23:56 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,164 @@
+task 1
+chap 1 to 6
+
+console.log;
+
+// var a = 5;
+// var b = 10;
+// var c = a+b;
+// alert(c);
+
+// Concatenation
+// var x = "taha";
+// var y = 15;
+// var z = x+y;
+// alert(z);
+
+// var f = "ahmed";
+// var g = 20;
+// var h = f*g;
+// alert(h);
+
+// var a = "5";
+// var b = 5;
+// var c = a+b;
+// alert(c);
+
+// var x = "10";
+// var y = 20;
+// var z = x+y;
+// alert(z);
+
+// var a = 20;
+// var a = 30;
+// alert(a);
+
+// var x = (10+4)/2*3-5;
+// alert(x);
+
+// var x = 5;
+// var y = x++
+// alert(y);
+
+// var a = 10;
+// var b = ++a;
+// alert(b);
+
+// var x = 3;
+// var y = x + ++x - x-- + ++x;
+
+// alert(y);
+
+
+
+// var a = 5;
+// var b = a++ - --a + a - a-- + ++a - a + a++ - ++a - --a;
+
+// alert(b);
+
+
+// var a = 9;
+// var b = a%3;
+// alert(b);
+
+// var x = Number (prompt("Enter Value","Your value is:"));
+// var x = pasreInt (prompt("Enter Value","Your value is:"));
+// var y = 10;
+
+task 2
+
+// let maths,chem,urdu,history,English;
+// maths=+prompt("Enter Maths marks" , "Input Number");
+// chem=+prompt("Enter Chem marks" , "Input Number");
+// urdu=+prompt("Enter urdu marks" , "Input Number");
+// history=+prompt("Enter history marks" , "Input Number");
+// English=+prompt("Enter english marks" , "Input Number");
+
+// var summarks= maths+chem+urdu+history+English;
+// alert(summarks);
+
+// var percentage=(summarks * 100)/500;
+
+
+// if (percentage>90)
+// {
+// alert("Your grade is = A+")
+// }
+
+// else if (percentage>80)
+// {
+// alert("Your grade is = A")
+// }
+
+// else if (percentage>70)
+// {
+// alert("Your grade is = B")
+// }
+
+// else if (percentage>60)
+// {
+// alert("Your grade is = C")
+// }
+
+// else if (percentage>50)
+// {
+// alert("Your grade is = D")
+// }
+
+
+// else if (percentage>40)
+// {
+// alert("Your grade is = E")
+// }
+
+// else if (percentage<40)
+// {
+// alert("You are failed!!")
+// }
+
+// else{
+// alert("wrong values");
+// }
+
+task 3
+
+// console.log("javascript")
+
+// var arr =["lion","rat","cat","dog"];
+// undefined
+// arr.length
+
+// arr.push("piegon");
+
+// arr.pop();
+// 'piegon'
+// arr.unshift("parrot")
+
+// arr.unshift();
+// var arr = ["lion","cow","donkey","piegon","owl","cat","dog"]
+// var arr = arr.splice(1,2,"piegon","donkey")
+// console.log(arr);
+// var arr1 =arr.splice(2,4);
+// var arr = ["wahab","Mustufa","Sufiyan","Ibrahim"]
+
+// alert (arr[1,2]);
+// document.write("Hello");
+// arr[1]
+
+// document.write(arr[0] + true);
+// ar a = ["SM0001","SM0002","SM0003","SM0004","SM0005","SM0006","SM0007","SM0008","SM0009","SM0010","SM0011","SM0012","SM0013","SM0014","SM0015","SM0014","SM0015","SM0016","SM0017","SM0018","SM0019","SM0020"];
+// var b = prompt("Search Roll number");
+// var flag = false;
+
+// for (var i =0;i < a.length; i++){
+// if (a[i]== b){
+// flag = true;
+// break;
+// }
+// }
+// if (flag) {
+// alert("Roll num found")
+// }else {
+// alert("Roll num not found ")
+// }
+
